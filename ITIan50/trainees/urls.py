f62fa4b9b4a5c79789824d743b24422e3358dd86
Jeihad <jeihadbaleegh1@gmail.com>
2025-03-20 17:46:13 +0200

    update and delete Generic

--- ITIan50/trainees/urls.py
+++ ITIan50/trainees/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TraineeListView, TraineeCreateView
+from .views import TraineeListView, TraineeCreateView, TraineeUpdateView, TraineeDeleteView
 
 urlpatterns = [
     path('', TraineeListView.as_view(), name='trainee-list'),
     path('add/', TraineeCreateView.as_view(), name='trainee-add'),
+    path('update/<int:pk>/', TraineeUpdateView.as_view(), name='trainee-update'),
+    path('delete/<int:pk>/', TraineeDeleteView.as_view(), name='trainee-delete'),
 ]
--- ITIan50/trainees/views.py
+++ ITIan50/trainees/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Trainee
 
 
@@ -13,4 +13,17 @@ class TraineeCreateView(CreateView):
     model = Trainee
     template_name = "trainees/trainee_form.html"
     fields = ["first_name", "last_name", "email"]
+    success_url = reverse_lazy("trainee-list")
+
+
+class TraineeUpdateView(UpdateView):
+    model = Trainee
+    template_name = "trainees/trainee_form.html"
+    fields = ["first_name", "last_name", "email"]
+    success_url = reverse_lazy("trainee-list")
+
+
+class TraineeDeleteView(DeleteView):
+    model = Trainee
+    template_name = "trainees/trainee_confirm_delete.html"
     success_url = reverse_lazy("trainee-list")
